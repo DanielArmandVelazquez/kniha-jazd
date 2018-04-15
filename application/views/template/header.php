0f34deea83e4f0d79e7a7fd71a7a58f2b200d697
DanielArmandVelazuqez <empower1>
2018-04-15 15:18:03 +0200

    pridane assets

---
+++ application/views/template/header.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Carlos
+ * Date: 15.04.2018
+ * Time: 15:07
+ */
+    c. <!-- Bootstrap core CSS -->
+<link href="<?php echo
+base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
+<!-- Bootstrap theme -->
+<link href="<?php echo base_url();?>assets/css/bootstraptheme.min.css"
+rel="stylesheet">
